6bee699cd8e4c0909d181098229d2ddf9b32e78c
Administrator <Administrator@DESKTOP-EK117L1>
2023-04-27 10:36:23 +0800

    使用TagHelper修改Index中的查看和图片添加项目文件。

---
+++ StudentManagement/Models/MockStudentRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Models
+{
+    public class MockStudentRepository : IStudentRepository
+    {
+        private List<Student> _students;
+        public MockStudentRepository()
+        {
+            _students = new List<Student>() { 
+                new Student(){Id=1,Name="刘玄德",ClassName="三年级",Email="liubei@163.com"},
+                new Student(){Id=2,Name="关云长",ClassName="二年级",Email="guanyu@163.com"},
+                new Student(){Id=3,Name="张翼德",ClassName="一年级",Email="张飞@163.com"},
+            };
+        }
+
+        public IEnumerable<Student> GetAllStudents()
+        {
+            return _students;
+        }
+
+        public Student GetStudent(int id)
+        {
+            return _students.FirstOrDefault(s => s.Id == id);
+        }
+    }
+}
